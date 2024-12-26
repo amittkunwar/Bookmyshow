9497767acca20fb28eaf2c3943d8d76532cfde61
adig03 <149831123+adig03@users.noreply.github.com>
2024-12-26 16:37:45 +0530

    Fetched Movie Data Successfully

--- app/src/main/java/com/example/bookmyshow/api/OMDBService.java
+++ app/src/main/java/com/example/bookmyshow/api/OMDBService.java
@@ -1,16 +1,21 @@
 package com.example.bookmyshow.api;
 
-import com.example.bookmyshow.Models.MovieResponse;
+import com.example.bookmyshow.Models.MovieDetails;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface TMDBService {
-    @GET("movie/popular")
-    Call<MovieResponse> getPopularMovies(
-            @Query("api_key") String apiKey,
-            @Query("language") String language,
-            @Query("page") int page
+
+
+
+public interface OMDBService {
+    @GET("/")
+    Call<MovieDetails> getMovieDetails(
+            @Query("t")
+            String movieName,
+            @Query("apikey")
+            String apiKey
     );
 }
+
